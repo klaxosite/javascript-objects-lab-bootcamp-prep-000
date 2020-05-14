9b9292596757c5f4c2345efd1dc6ff392ed38bfc
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 04:14:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var recipes = new Object({})
 function updateObjectWithKeyAndValue(object, key, value){
   return object
   return Object.assign({}, obj, { [key]: value})
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return Object
-  
+
 
 }
 
