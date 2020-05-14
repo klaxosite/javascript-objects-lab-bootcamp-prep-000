85f8fe4f8617932538018ac52563dcb3a6f19d7e
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 03:59:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var 
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
