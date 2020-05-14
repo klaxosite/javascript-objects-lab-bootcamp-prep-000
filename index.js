2ec0891314086606e3f1aad16f7d5003aacab515
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 04:38:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object({})
 
 function updateObjectWithKeyAndValue(object, key, value){
-  update = Object.assign({}, object)
+  var update = Object.assign({}, object)
   update[key] = value;
   return update
 
